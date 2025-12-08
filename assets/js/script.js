b4c87e6bce0e3dbe95ed4d5c1be0a00f246cbd76
jayaram <sriramselvam016@gmail.com>
2025-12-08 09:45:00 +0530

    Fixed all corection

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,3 +67,37 @@ $(".testi1").owlCarousel({
         
     }
 })
+$(".team").owlCarousel({
+    center: true,
+    nav:false,
+    slideBy:1,
+    autoplay: true,
+    items: 3,
+    margin:20,
+    // animateIn:'fadeIn',
+    // animateOut:'fadeOut',
+    loop:true,
+    responsive:{
+        0:{
+            items:1,
+            nav: false,
+            dots:false
+        },
+        600:{
+            items:1,
+            nav:false,
+            dots:false
+        },
+        900:{
+            items:1,
+            nav: false,
+            dots:false
+        }
+        ,1000:{
+            items:1,
+            nav: false,
+            dots:false
+        }
+        
+    }
+})
